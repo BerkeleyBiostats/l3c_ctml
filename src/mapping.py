993b56d6eeb05e4b753db5b4f6d08350ac2e3099
SeraphinaShi <junmingshi67@gmail.com>
2023-04-10 10:00:59 -0700

    change order

--- src/mapping.py
+++ src/mapping.py
@@ -1,16 +1,17 @@
 
 # 1_cohort_and_features
-Hospitalized_Cases = spark.sql(sql_statement_04())
-Collect_the_Cohort = spark.sql(sql_statement_00())
 Covid_Pasc_Index_Dates = spark.sql(sql_statement_01()) 
 
+Collect_the_Cohort = spark.sql(sql_statement_00())
 long_covid_patients = spark.sql(sql_statement_08()) 
 
+Hospitalized_Cases = spark.sql(sql_statement_04())
+
 hosp_and_non = spark.sql(sql_statement_06()) 
+
 Feature_Table_Builder_v0 = spark.sql(sql_statement_03()) 
 ICU_visits = spark.sql(sql_statement_05()) 
 inpatient_visits = spark.sql(sql_statement_07()) 
-
 
 tot_icu_days_calc = spark.sql(sql_statement_09()) 
 tot_ip_days_calc = spark.sql(sql_statement_10()) 
